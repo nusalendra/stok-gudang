25b249507e42e903c86251f935f1fa217409d668
Nusalendra <nusalendraa@gmail.com>
2024-10-25 16:42:08 +0700

    feat:laporan harian

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Barang;
+use App\Models\BarangKeluar;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class LaporanController extends Controller
 {
     public function index()
     {
-        return view('content.pages.admin.laporan.index');
+        $data = Barang::with('barangKeluar')->get();
+
+        foreach ($data as $item) {
+            $stokKeluarHarian = BarangKeluar::where('barang_id', $item->id)
+                ->whereDate('created_at', Carbon::now()->toDateString())
+                ->sum('jumlah');
+
+            $pendapatanHarian = BarangKeluar::where('barang_id', $item->id)
+                ->whereDate('created_at', Carbon::now()->toDateString())
+                ->sum('pendapatan');
+
+            $item->stok_keluar_harian = $stokKeluarHarian;
+            $item->pendapatan_harian = $pendapatanHarian;
+        }
+        return view('content.pages.admin.laporan.index', compact('data'));
     }
 }
